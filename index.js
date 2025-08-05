789c27e6e62c20e8af4df49d64f1a9bcfd47d77e
Rohat FİLİTOĞLU <rohatfilitoglu06@gmail.com>
2025-08-05 15:48:04 +0300

    update

--- index.js
+++ index.js
@@ -11,14 +11,20 @@ const favoritesRoutes = require('./routes/favorites');
 const userRoutes = require('./routes/users');
 
 const app = express();
-app.use(cors());
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
+app.use(cors(corsOpts));
 app.use(express.json());
 
 app.use('/favorites', favoritesRoutes);
