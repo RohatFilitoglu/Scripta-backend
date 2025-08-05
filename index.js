831e92e892e2dacdaf23b5f61a07a8a6c67146ca
Rohat FİLİTOĞLU <rohatfilitoglu06@gmail.com>
2025-08-05 14:31:18 +0300

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 // ✅ CORS'u burada uyguluyoruz
 app.use(cors({
-  origin: 'https://scripta-backend-7bznrk0c7-rohat-filitoglus-projects.vercel.app', // Frontend'in çalıştığı domain
+  origin: 'https://scripta-frontend-sand.vercel.app/', // Frontend'in çalıştığı domain
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
