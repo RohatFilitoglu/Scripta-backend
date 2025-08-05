9e5ff27d6306f1c566fb39d335adc79a656cba13
Rohat FİLİTOĞLU <rohatfilitoglu06@gmail.com>
2025-08-05 15:46:13 +0300

    update

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const userRoutes = require('./routes/users');
 const app = express();
 app.use(cors());
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
 });
 app.use(express.json());
 
