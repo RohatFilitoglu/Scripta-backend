8820bbade4bb4d714348242ef66b015a80978e94
Rohat FİLİTOĞLU <rohatfilitoglu06@gmail.com>
2025-08-05 16:27:18 +0300

    update

--- index.js
+++ index.js
@@ -12,21 +12,7 @@ const userRoutes = require('./routes/users');
 
 const app = express();
 
-const corsOptions = {
-    origin: 'https://scripta-frontend-sand.vercel.app', // Allow only this domain
-    methods: 'GET,POST,PUT,DELETE', // Allow only specific HTTP methods
-    allowedHeaders: 'Content-Type,Authorization', // Allow specific headers]
-    credentials: true
-};
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-
-app.use('routes/',cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use('/favorites', favoritesRoutes);
