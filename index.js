37c73ee7358f22ae104b78d9b8985c2fc2aa06e7
Rohat FİLİTOĞLU <rohatfilitoglu06@gmail.com>
2025-08-06 11:03:54 +0300

    update

--- index.js
+++ index.js
@@ -12,7 +12,22 @@ const userRoutes = require('./routes/users');
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'https://your-frontend.vercel.app',
+  'http://localhost:3000'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/favorites', favoritesRoutes);
