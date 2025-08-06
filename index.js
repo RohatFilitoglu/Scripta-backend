a9e92465147b4e6f3441e38733ca11c1352fbe73
Rohat FİLİTOĞLU <rohatfilitoglu06@gmail.com>
2025-08-06 11:14:44 +0300

    update

--- index.js
+++ index.js
@@ -18,13 +18,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: 'https://scripta-frontend-sand.vercel.app/', // <-- Buraya kendi frontend domainini yaz!
   credentials: true
 }));
 
