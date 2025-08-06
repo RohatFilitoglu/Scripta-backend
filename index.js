1ced0712707fdeb7cd90d1cbc3bbb6d2a5a09652
Rohat FİLİTOĞLU <rohatfilitoglu06@gmail.com>
2025-08-06 14:49:33 +0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const swaggerUi = require('swagger-ui-express');
-const serverless = require('serverless-http');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const serverless = require("serverless-http");
 
-const swaggerSpec = require('./swagger');
-const postRoutes = require('./routes/posts');
-const commentRoutes = require('./routes/comments');
-const favoritesRoutes = require('./routes/favorites');
-const userRoutes = require('./routes/users');
+const swaggerSpec = require("./swagger");
+const postRoutes = require("./routes/posts");
+const commentRoutes = require("./routes/comments");
+const favoritesRoutes = require("./routes/favorites");
+const userRoutes = require("./routes/users");
 
 const app = express();
 
@@ -16,20 +16,13 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/favorites', favoritesRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/posts', postRoutes);
-app.use('/comments', commentRoutes);
-app.use('/profiles', userRoutes);
+app.use("/favorites", favoritesRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/posts", postRoutes);
+app.use("/comments", commentRoutes);
+app.use("/profiles", userRoutes);
 
-if (process.env.VITE_LOCAL === 'true') {
-  // 🌐 LOCAL çalıştırmak için: .env dosyana LOCAL=true ekle
-  const PORT = process.env.PORT || 3001;
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-} else {
-  // 🚀 Vercel için export
-  module.exports = app;
-  module.exports.handler = serverless(app);
-}
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
